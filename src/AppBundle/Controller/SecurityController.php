8af5c5434011a71569e02b5b248a91489791422e
sruj <chryplewiczpawel@wp.pl>
2017-04-28 14:14:06 +0200

    logout, login, redirect, authorization, authentication.

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -15,10 +15,8 @@ class SecurityController extends Controller
     {
         $authenticationUtils = $this->get('security.authentication_utils');
 
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', array(
