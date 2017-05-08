fb08ce80f0a30a8228c4f144a269498cfd23c044
sruj <chryplewiczpawel@wp.pl>
2017-05-08 03:05:51 +0200

    Chat. Serwer WebSocket, Klasa aplikacji Ratchet, Widok z javascript u klienta.

--- src/AppBundle/Controller/ChatController.php
+++ src/AppBundle/Controller/ChatController.php
@@ -2,16 +2,51 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\CssSelector\Tests\Parser\ReaderTest;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChatController extends Controller
 {
+//    /**
+//     * @Route("/chat", name="chat")
+//     */
+//    public function indexAction()
+//    {
+//        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+//            throw $this->createAccessDeniedException();
+//        }
+//
+//        $user = $this->getUser();
+//
+//        return $this->render('@App/chat/index.html.twig');
+//    }
+
     /**
      * @Route("/chat", name="chat")
      */
-    public function indexAction()
+    public function chatAction(Request $request)
     {
-        return $this->render('chat/index.html.twig');
+        $defaultData = array('message' => 'Type your message here');
+        $form = $this->createFormBuilder($defaultData)
+            ->add('name', \Symfony\Component\Form\Extension\Core\Type\TextType::class)
+            ->add('send', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $username = $data['name'];
+            return $this->render('chat.html.twig', ['username'=>$username]);
+        }
+
+        return $this->render('@App/chat/chat.html.twig',['form' => $form->createView()]);
+
+
     }
+
 }
---
+++ src/AppBundle/Websocket/Chat.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chiny
+ * Date: 2017-05-01
+ * Time: 19:57
+ */
+
+namespace AppBundle\Websocket;
+
+use Psr\Log\LoggerInterface;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+
+
+/**
+ * Class Chat
+ *
+ * - This class listen 4 events
+ */
+class Chat implements MessageComponentInterface
+{
+    protected $clients;
+    protected $userlist;
+    private $logger;
+
+
+    /**
+     * Chat constructor.
+     */
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+        $this->userlist = [];
+
+        $this->logger = new Logger('name');
+    }
+
+
+    /**
+     * Called when a new client has Connected.
+     * Store the new connection to send messages to later.
+     */
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+
+
+    /**
+     * Called when a message is received by a Connection
+     */
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        $msgDecode = json_decode($msg);
+        $flag = $msgDecode->message->flag;
+        $userId = $from->resourceId;
+
+        if($flag == "newUsername"){
+            $username = $msgDecode->message->data;
+            $this->addNewUsernameToUserList($username,$userId);
+            $jsonUserList = json_encode($this->userlist);
+            $jsonMsg = $this->prepareMessage("userlist", $jsonUserList);
+            $this->sendUserListToEveryClient($jsonMsg);
+
+            return;
+        }
+
+        if($flag == "chatMessage") {
+            $numRecv = count($this->clients) - 1;
+            echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+                , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+            foreach ($this->clients as $client) {
+                $client->send($msg);
+            }
+        }
+    }
+
+
+    private function addNewUsernameToUserList($username,$userId)
+    {
+        //logger. Zbędne raczej.
+//        $this->logger->pushHandler(new StreamHandler(__DIR__.'chryp.log', Logger::NOTICE));
+//        $this->logger->notice($message);
+
+        $this->userlist[$userId] = $username;
+
+        return true;
+    }
+
+
+
+    /**
+     * Called when a Connection is closed
+     */
+    public function onClose(ConnectionInterface $conn)
+    {
+        // The connection is closed, remove it, as we can no longer send it messages
+        $this->clients->detach($conn);
+
+        echo "Connection {$conn->resourceId} has disconnected\n";
+
+        $this->removeClient($conn->resourceId);
+        $jsonUserList = json_encode($this->userlist);
+        $jsonMsg = $this->prepareMessage("userlist", $jsonUserList);
+        $this->sendUserListToEveryClient($jsonMsg);
+
+    }
+
+    private function removeClient($userId){
+        //usuń element z tablicy
+        unset($this->userlist[$userId]);
+    }
+
+
+    /**
+     * Called when an error occurs on a Connection
+     */
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "An error has occurred: {$e->getMessage()}\n";
+
+        $conn->close();
+    }
+
+
+
+
+
+
+
+
+
+
+
+    private $debugCounter = 0;
+    private function debug($from)
+    {
+        $this->debugCounter++;
+        $filename= (string)date('Y-m-d_H.i.s');
+        ob_start();
+        var_dump($from);
+        $result = ob_get_clean();
+        file_put_contents(__DIR__ .'_'.$filename.'_('.$this->debugCounter.').txt', $result);
+    }
+
+    /**
+     * @param $flag
+     * @param $data
+     * @return string
+     */
+    private function prepareMessage($flag, $data): string
+    {
+        $jsonMsg = '{"message":{ "flag":"' . $flag . '", "data": ' . $data . '}}';
+        return $jsonMsg;
+    }
+
+    /**
+     * @param $jsonMsg
+     */
+    private function sendUserListToEveryClient($jsonMsg)
+    {
+        foreach ($this->clients as $client) {
+            $client->send($jsonMsg);
+        }
+    }
+
+
+}
