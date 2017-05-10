6cd43ab873c852cb32e59775107a3f39753953ce
sruj <chryplewiczpawel@wp.pl>
2017-05-10 17:10:35 +0200

    added Symfony command chat:server

--- src/AppBundle/Command/ServerCommand.php
+++ src/AppBundle/Command/ServerCommand.php
@@ -1,18 +1,16 @@
-<!--TODO: PRZENIEŚĆ SKRYPT SERWERA TU-->
 <?php
+
+namespace AppBundle\Command;
+
 use Ratchet\Server\IoServer;
 use AppBundle\Websocket\Chat;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
-
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 
-/*
- *  Tu tak robili: http://stackoverflow.com/questions/17529657/how-to-use-properly-websockets-in-symfony2
- */
 class ServerCommand extends ContainerAwareCommand
 {
     protected function configure()
@@ -24,8 +22,15 @@ class ServerCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $chat = $this->getContainer()->get('chat');
-        $server = IoServer::factory(new HttpServer(new WsServer($chat)), 8080);
+        $server = IoServer::factory(
+            new HttpServer(
+                new WsServer(
+                    new Chat()
+                )
+            ),
+            8080
+        );
+
         $server->run();
     }
 }
